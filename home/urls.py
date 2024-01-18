c470919b8e50261709d13af5fe74cbda2acb2414
Sarthak <sarthakrangari788@gmail.com>
2024-01-18 21:47:16 +0530

    tribe work

--- home/urls.py
+++ home/urls.py
@@ -3,12 +3,11 @@ from .views import *
 
 
 urlpatterns = [
-
-    path('<slug>/<int:year>/',tribe_detail_view,name='tribe_detail'), 
-    path('test/',test_view,name='test'),
-    path('form/',form_view,name='form'),
-    path('<slug>/pdf/',tribe_pdf_view,name='tribe_pdf'), 
-
+    # path('test/', test_view, name='test'),
+    path('form/', form_view, name='form'),
+    path('<slug>/pdf/', tribe_pdf_view, name='tribe_pdf'),
+    path('<slug1>/<slug2>/', tribe_detail_view, name='tribe_detail'), 
 ]
+
 
 
