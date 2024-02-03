5daa03de792c20c8cba5fb72cbf5a2b171ab885b
Sarthak <sarthakrangari788@gmail.com>
2024-02-03 15:56:56 +0530

    code test.html calculationn

--- home/urls.py
+++ home/urls.py
@@ -3,10 +3,10 @@ from .views import *
 
 
 urlpatterns = [
-    # path('test/', test_view, name='test'),
     path('tribe-form/', tribe_form_view, name='tribe_form'),
     path('<slug>/pdf/', tribe_pdf_view, name='tribe_pdf'),
     path('<name>/<year>/', tribe_detail_view, name='tribe_detail'), 
+    path('test/', test_view, name='test'),
 ]
 
 
